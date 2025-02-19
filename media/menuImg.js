627e326406f4215218bda051f8f2564d78fdc2b5
pengemis-sad <156815366+pengemis-sad@users.noreply.github.com>
2025-02-19 14:58:53 +0700

    Add files via upload

---
+++ media/menuImg.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+module.export = [
+fs.readFileSync('./media/lookk.jpg'),
+fs.readFileSync('./media/sadd.jpg'),
+fs.readFileSync('./media/reply.jpg'),
+fs.readFileSync('./media/smile.jpg'),
+fs.readFileSync('./media/menu1.jpg'),
+fs.readFileSync('./media/menu2.jpg'),
+fs.readFileSync('./media/menu3.jpg'),
+fs.readFileSync('./media/menu4.jpg'),
+fs.readFileSync('./media/menu5.jpg'),
+fs.readFileSync('./media/menu6.jpg'),
+fs.readFileSync('./media/menu7.jpg'),
+fs.readFileSync('./media/menu8.jpg'),
+fs.readFileSync('./media/menu9.jpg')
+];
